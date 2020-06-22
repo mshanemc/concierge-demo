b423b00fb534c13f283e98c05189dff509ce7efc
mshanemc <shane.mclaughlin@salesforce.com>
2020-06-22 17:44:28 -0500

    longer demos

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,5 @@
 # sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
-sfdx shane:org:create -f config/snapshot-project-scratch-def.json -d 1 -s -n --userprefix concierge --userdomain work.shop
+sfdx shane:org:create -f config/snapshot-project-scratch-def.json -d 7 -s -n --userprefix concierge --userdomain work.shop
 
 # to get access to audit field create
 sfdx force:source:deploy -p securityStuff/
