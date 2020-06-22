af90addbfcc149fd05f7bd0ba6336e6f91ad2c2a
mshanemc <shane.mclaughlin@salesforce.com>
2020-06-22 10:22:00 -0500

    audit fields

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,8 @@
 # sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
 sfdx shane:org:create -f config/snapshot-project-scratch-def.json -d 1 -s -n --userprefix concierge --userdomain work.shop
+
+# to get access to audit field create
+sfdx force:source:deploy -p securityStuff/
 sfdx force:data:record:update -s User -w "Name='User User'" -v "UserPermissionsKnowledgeUser=true"
 
 sfdx force:source:push
