3ff7d83a9f52c623633f31f2e032bb3b35dd7542
mshanemc <shane.mclaughlin@salesforce.com>
2020-07-29 09:28:22 -0500

    update namespace

--- orgInit.sh
+++ orgInit.sh
@@ -10,7 +10,7 @@ sfdx force:source:push
 sfdx force:user:permset:assign -n solutions
 
 # add a background option to settings
-sfdx force:data:record:create -s cncrgdemo__Background_Settings__c -v "cncrgdemo__Background_File_Type__c=Image cncrgdemo__File_Location__c=newBackground Name=NewBackground"
+sfdx force:data:record:create -s cncrgdemo2__Background_Settings__c -v "cncrgdemo2__Background_File_Type__c=Image cncrgdemo2__File_Location__c=newBackground Name=NewBackground"
 sfdx force:data:bulk:upsert -f data/Knowledge__kav.csv -i id -s Knowledge__kav --wait 30
 
 sfdx force:apex:execute -f scripts/conciergeSetup.cls
@@ -18,8 +18,8 @@ sfdx force:apex:execute -f scripts/conciergeSetup.cls
 sfdx shane:data:favorite -o Knowledge__kav -w "title='Flickering Monitor'"
 sfdx shane:listview:favorite -t Open_IT_Tickets -o Case
 sfdx shane:listview:favorite -t published_articles -o Knowledge__kav -l "Published Articles" 
-sfdx shane:tab:favorite -t cncrgdemo__Concierge_LEX_Tab -l "Conciege in Lightning"
-sfdx shane:tab:favorite -t cncrgdemo__Concierge -l "Conciege Standalone"
-sfdx shane:tab:favorite -t cncrgdemo__Settings_Page -l "Conciege Settings"
+sfdx shane:tab:favorite -t cncrgdemo2__Concierge_LEX_Tab -l "Conciege in Lightning"
+sfdx shane:tab:favorite -t cncrgdemo2__Concierge -l "Conciege Standalone"
+sfdx shane:tab:favorite -t cncrgdemo2__Settings_Page -l "Conciege Settings"
 
-sfdx force:org:open -p lightning/n/cncrgdemo__Concierge_LEX_Tab
+sfdx force:org:open -p lightning/n/cncrgdemo2__Concierge_LEX_Tab
